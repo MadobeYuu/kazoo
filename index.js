8f8d52875a42aebdcdcc6a25ca16f6b7d1b3f274
MadobeYuu <aocgvrv@gmail.com>
2025-09-21 15:39:35 +0300

    hz

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ let ads = []
 let app = express()
 app.use(express.static("static"))
 app.use(express.json())
+app.set("view engine", "ejs")
+app.set("views", "views")
 
 app.get("/", (req, res)=>{
-    res.send("Hello")
+    res.render("index", {products: ads})
 })
 
 app.post("/add", (req, res)=>{
@@ -14,8 +16,9 @@ app.post("/add", (req, res)=>{
     res.send({status: "ok"})
 })
 
-app.get("/ads", (req,res)=>{
-    res.json(ads)
+app.get((req, res, next)=>{
+    res.status = 404
+    res.render("notFound")
 })
 
 app.listen(3000, ()=>console.log("Server on!"))
